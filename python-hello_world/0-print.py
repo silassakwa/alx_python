6d5eb830ac41b8dfd4b4aed3ee12d06e0ac58a15
silassakwa <silassakwa2016@gmail.com>
2023-07-19 16:34:39 +0300

    o-print

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,3 +1,2 @@
 #print("Programming is like building a multilingual puzzle")
-for num in range(99):
-    print("{0}, {1}".format(num))
+print("Programming is like building a multilingual puzzle",end="\n")
