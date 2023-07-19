17e401b55bc366176f5aebb295d0b3a368feae76
silassakwa <silassakwa2016@gmail.com>
2023-07-19 16:04:01 +0300

    3-string

---
+++ python-functions/3-string.py
@@ -0,0 +1,6 @@
+'''
+Write a Python function called reverse_string that takes a string as input and returns the reverse of that string.
+'''
+def reverse_string(string):
+    return string[::-1]
+print(reverse_string("hello"))
