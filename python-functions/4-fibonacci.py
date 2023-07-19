ed1ef260c08c0a83fdad95841e013e7bcb9123ac
silassakwa <silassakwa2016@gmail.com>
2023-07-19 16:08:48 +0300

    4-fibonaccii series

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,15 @@
+'''
+Write a Python function called fibonacci_sequence that takes a number n as input and returns a list of the first n Fibonacci numbers.
+'''
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+
+    fibonacci_numbers = [0, 1]
+
+    while len(fibonacci_numbers) < n:
+        next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
+        fibonacci_numbers.append(next_number)
+
+    return fibonacci_numbers[:n]
+print(fibonacci_sequence(0))
