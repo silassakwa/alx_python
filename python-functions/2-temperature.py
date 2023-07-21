6c2d5b662ca88b67c8618b9df15595c3df38d6e3
silassakwa <silassakwa2016@gmail.com>
2023-07-21 14:27:40 +0300

    2-temprature python

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -5,6 +5,3 @@ def convert_to_celsius(fahrenheit):
 convert_to_celsius = __import__('2-temperature').convert_to_celsius
 
 print(convert_to_celsius(100))
-print(convert_to_celsius(-40))
-print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
