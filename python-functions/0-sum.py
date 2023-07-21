948bf85cdc1305ef147d5e81ab8f5974773671ea
silassakwa <silassakwa2016@gmail.com>
2023-07-21 11:04:13 +0300

    0-sum python

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
+#add = __import__('0-sum').add
 def add(a,b):
     return a+b
-print(add(1,2))
-print(add(98,0))
-print(add(100,-2))
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
