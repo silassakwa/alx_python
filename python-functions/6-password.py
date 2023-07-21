d4ee34f600531271945c0c1e21f107e9b517d9fe
silassakwa <silassakwa2016@gmail.com>
2023-07-21 12:55:49 +0300

    1-power python

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,6 +1,7 @@
 def validate_password_length(password):
     return len(password) >= 8
 
+
 # Test cases for password length validation
 print(validate_password_length("Password123"))  # True
 print(validate_password_length("abc123"))       # False
