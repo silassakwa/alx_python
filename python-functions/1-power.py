f2531892d490bae7120c459caee9e435d01bb1f3
silassakwa <silassakwa2016@gmail.com>
2023-07-21 12:43:52 +0300

    1-power python

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,6 +1,10 @@
 def pow(a, b):
     return a**b
 #!/usr/bin/env python3
-
+pow = __import__('1-power').pow
 
 print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,35 +1,8 @@
-def validate_password(password):
-    # Check if the password is at least 8 characters long
-    if len(password) < 8:
-        return False
+def validate_password_length(password):
+    return len(password) >= 8
 
-    # Check if the password contains at least one uppercase letter, one lowercase letter, and one digit
-    has_uppercase = False
-    has_lowercase = False
-    has_digit = False
-
-    for char in password:
-        if char.isupper():
-            has_uppercase = True
-        elif char.islower():
-            has_lowercase = True
-        elif char.isdigit():
-            has_digit = True
-
-    if not (has_uppercase and has_lowercase and has_digit):
-        return False
-
-    # Check if the password contains spaces
-    if " " in password:
-        return False
-
-    # If all checks pass, return True
-    return True
-
-#!/usr/bin/env python3
-validate_password = __import__('6-password').validate_password
-
-print(validate_password("Password123"))
-print(validate_password("abc123"))
-print(validate_password("Password 123"))
-print(validate_password("password123"))
+# Test cases for password length validation
+print(validate_password_length("Password123"))  # True
+print(validate_password_length("abc123"))       # False
+print(validate_password_length("12345678"))    # True
+print(validate_password_length("short"))       # False
