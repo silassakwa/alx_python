89252cf76de3171cf4f35cc23298fb8e4dceb1bb
silassakwa <silassakwa2016@gmail.com>
2023-07-24 17:35:53 +0300

    python raise exception

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,9 +1,13 @@
 def raise_exception_msg(message=""):
     raise NameError(message)
 
-raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
 
-try:
-    raise_exception_msg("C is fun")
-except NameError as ne:
-    print(ne)
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
+
+    try:
+        raise_exception_msg("C is fun")
+    except NameError as ne:
+        print(ne)
