399149235897074d07983a64f81281660a80d2e1
silassakwa <silassakwa2016@gmail.com>
2023-07-25 14:16:27 +0300

    python data structure list and tuple 2-no

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,9 @@
+def multiple_returns(sentence):
+    length = len(sentence)
+    first = sentence[0] if length > 0 else None
+    return length, first
+
+if __name__ == "__main__":
+    sentence = "At Holberton school, I learnt C!"
+    length, first = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(length, first))
