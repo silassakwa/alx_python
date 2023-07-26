ff153e185f0bf2abe501b57b862bbc7f06cb6ebc
silassakwa <silassakwa2016@gmail.com>
2023-07-26 14:14:17 +0300

    Set, Dictionary

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,9 @@
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
+    # Create a new matrix to store the squared values
+    new_matrix = [[num ** 2 for num in row] for row in matrix]
+    return new_matrix
+
     for row in matrix:
         new_row = []
         for num in row:
