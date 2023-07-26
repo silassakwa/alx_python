83b994d19cb1dc9514aac8699c4c6f9d227c66a5
silassakwa <silassakwa2016@gmail.com>
2023-07-26 13:10:56 +0300

    python import

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,11 @@
 #import tha add_0 module 
 from add_0 import add
 #declare the two given variable
-a = 1
-b = 2
+def main():
+    a = 1
+    b = 2
 
-# Print the result using string formatting
-print("{} + {} = {}".format(a, b, add(a,b)))
+    # Print the result using string formatting
+    print("{} + {} = {}".format(a, b, add(a,b)))
 if __name__ == "__main__":
-    pass
+    main()
