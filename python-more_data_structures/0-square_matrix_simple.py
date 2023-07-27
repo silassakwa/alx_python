4509c1a957cea66c16ab43c7c094092e8941d174
silassakwa <silassakwa2016@gmail.com>
2023-07-27 13:41:14 +0300

    0-square_matrix_simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,25 +1,13 @@
-
 def square_matrix_simple(matrix=[]):
-    # Create a new matrix to store the squared values
-    new_matrix = [[num ** 2 for num in row] for row in matrix]
-    return new_matrix
+    return [[num ** 2 for num in row] for row in matrix]
 
-    for row in matrix:
-        new_row = []
-        for num in row:
-            new_row.append(num ** 2)
-        new_matrix.append(new_row)
-    return new_matrix
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
 
-#!/usr/bin/python3
-square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix)
+    print(matrix)
