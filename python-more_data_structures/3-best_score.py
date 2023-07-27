a4f8b9570cf9fa236ece619b1d132209acd4ad88
silassakwa <silassakwa2016@gmail.com>
2023-07-27 13:23:15 +0300

    python-more_data_structures

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -5,7 +5,8 @@ def best_score(a_dictionary):
     max_score = max(a_dictionary.values())
     best_keys = [key for key, score in a_dictionary.items() if score == max_score]
     return best_keys[0]
-
+#!/usr/bin/python3
+best_score = __import__('3-best_score').best_score
 # Test case
 my_dict = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
 best_key = best_score(my_dict)
