304890ed1fb9c2770914a49bd59806bf27191f81
silassakwa <silassakwa2016@gmail.com>
2023-07-27 13:02:48 +0300

    python-more_data_structures

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python3
-
 def best_score(a_dictionary):
-    if a_dictionary:
-        return max(a_dictionary, key=a_dictionary.get)
-    return None
+    if not a_dictionary:
+        return None
 
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+    best_key = max(a_dictionary, key=a_dictionary.get)
+    return best_key
+
+# Test cases
+a_dictionary = {'John': 12, 'Bob': 30, 'Mike': 14, 'Molly': 16, 'Adam': 10}
 best_key = best_score(a_dictionary)
 print("Best score: {}".format(best_key))
 
