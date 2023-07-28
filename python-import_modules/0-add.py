62db416fd2c3cd54cd8f2ae79fde4222d3a40f0d
silassakwa <silassakwa2016@gmail.com>
2023-07-28 15:51:03 +0300

    200~0-add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,25 +1,5 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return a + b
-
-a = 1
-b = 2
-
-# Import the add function from add_0.py
-from add_0 import add
-
-# Calculate the sum
-result = add(a, b)
-
-# Print the result using string formatting
-if __name__ =="__main__":
-    print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,3 +1,10 @@
-#creatin a funtion to add two variables 
+#!/usr/bin/python3
 def add(a, b):
-    return a+b
+    """My addition function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
