173ee64fea206e8330da288a487b3efbc95111fc
silassakwa <silassakwa2016@gmail.com>
2023-07-29 12:34:32 +0300

    0-square creating a class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,5 +5,21 @@ New class Square
 
 
 class Square:
-    """ Empty class """
-    pass
+    def __init__(self, size):
+        self.__size = size
+
+# Test the Square class
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))  # Output: <class '__main__.Square'>
+    print(my_square.__dict__)  # Output: {'_Square__size': 3}
+
+    try:
+        print(my_square.size)  # Output: 'Square' object has no attribute 'size'
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)  # Output: 'Square' object has no attribute '__size'
+    except Exception as e:
+        print(e)
