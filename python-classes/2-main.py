9e58d742bf5146d3ee6c390f1cd43ea45acec382
silassakwa <silassakwa2016@gmail.com>
2023-08-01 14:44:59 +0300

    python class 2-square

---
+++ python-classes/2-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+Square = __import__('2-square').Square
+
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,54 +1,24 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
 class Square:
-    """
-    A class that defines a square.
+    """Represent a square."""
 
-    Attributes:
-        __size (int): The size of the square.
+    def __init__(self,size=0):
+      
+        """Initialize a new Square.
 
-    Methods:
-        __init__(self, size=0): Constructor method for Square class.
-            Initializes a square object with the given size.
-        area(self): Calculates and returns the area of the square.
-
-    Raises:
-        TypeError: If the input size is not an integer.
-        ValueError: If the input size is less than 0.
-    """
-
-    def __init__(self, size=0):
-        """initialize square
         Args:
-            size (int): size of the square
+
+            size (int): The size of the new square.
+
         """
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = size  # size of the square
+        if not isinstance(size,int):
+            raise TypeError("size must be integer")
+        elif size<0:
+            raise ValueError("size must be an integer")
+        self.size=size
 
     def area(self):
-        """calculate and print the area of the square"""
-        return self.__size ** 2
-   #!/usr/bin/python3
-"""
-2-main.py - A script to demonstrate the Square class functionality.
-
-This script demonstrates the usage of the Square class defined in the module 2-square.py.
-It creates instances of the Square class with different sizes and calculates their areas.
-
-Classes:
-    None
-
-Functions:
-    None
-"""
-
-# Importing the Square class from the module 2-square.py
-#Square = __import__('2-square').Square
-
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
-
-# my_square_2 = Square(5)
-# print("Area: {}".format(my_square_2.area()))
+        return self.size **2
+    
