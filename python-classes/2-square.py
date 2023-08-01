5ebd129eb456273bbcacab56030e99fd03e78bc7
silassakwa <silassakwa2016@gmail.com>
2023-08-01 09:57:41 +0300

    python classes

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -50,5 +50,5 @@ Functions:
 my_square_1 = Square(3)
 print("Area: {}".format(my_square_1.area()))
 
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
+# my_square_2 = Square(5)
+# print("Area: {}".format(my_square_2.area()))
