f26f5be29bd7928b06d82befca7c02e3493df755
silassakwa <silassakwa2016@gmail.com>
2023-08-02 12:28:54 +0300

    class inheritance 0-class

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -2,4 +2,5 @@
 """Write a function that returns True if the object is exactly an instance of the specified class ; otherwise False."""
 
 def is_same_class(obj, a_class):
+    """returns the list of available attributes"""
     return type(obj) is a_class
