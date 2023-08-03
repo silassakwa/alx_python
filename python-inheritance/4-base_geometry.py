89d3ea239f2cefd62e76029bfdb7cdac76580a91
silassakwa <silassakwa2016@gmail.com>
2023-08-03 11:43:23 +0300

    improved geometry

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" 4 base geometry new class"""
+class BaseGeometry:
+    """"" this is a new class called base geometry"""""
+    pass
+def area(self):
+    raise Exception("area() is not implemented")
