18c6757c5b84cc4d84418f079b9351a3031d7887
silassakwa <silassakwa2016@gmail.com>
2023-08-03 18:42:37 +0300

    python inheritance

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-""" module subclass rectangle
-"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Rectangle class Module"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle that inherits from BaseGeometry"""
+    """Rectangle class"""
     def __init__(self, width, height):
-        BaseGeometry.integer_validator(self, 'height', height)
-        self.__height = height
-        BaseGeometry.integer_validator(self, 'width', width)
+        """Initilize rectangle method"""
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
 
     def area(self):
+        """Method that returns area of rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
-
+        """Returns a string"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
