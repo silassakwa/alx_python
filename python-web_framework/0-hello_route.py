2aaef59cf9df1ac157fc392929747d0d3fae16d5
silassakwa <silassakwa2016@gmail.com>
2023-08-31 03:18:28 -0700

    flask hello world

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,3 +1,8 @@
+#!/user/bin/python
+"""
+flask web frame work to display hello HBNB!
+"""
+
 from flask import Flask
 
 app=Flask(__name__)
@@ -7,4 +12,4 @@ def home():
     return  " Hello HBNB!"
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port='5000',debug=True)
