6e3089450b49790d6a66980a0107291c49f71361
silassakwa <silassakwa2016@gmail.com>
2023-08-31 03:44:58 -0700

    a route with text

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,35 @@
+"""
+A simple Flask web application to display "Hello HBNB!" and custom messages.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def home():
+    """
+    The view function for the home page ("/") of the web application.
+
+    Returns:
+        str: The greeting message "Hello HBNB!".
+    """
+    return "Hello HBNB!"
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_c_text(text):
+    """
+    Display "C" followed by the value of the text variable.
+
+    Args:
+        text (str): The text variable from the URL.
+
+    Returns:
+        str: The message "C " followed by the value of text.
+    """
+    # Replace underscores with spaces
+    formatted_text = text.replace("_", " ")
+    return f"C {formatted_text}"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000', debug=True)
