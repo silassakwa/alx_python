ab07689f2bc9f5e7654599eac9c40d259271ccfe
silassakwa <silassakwa2016@gmail.com>
2023-09-29 04:42:23 -0700

    0-main.py

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module that defines the Base class """
+
+class Base:
+    """ Base class for managing id attribute """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initialize the Base instance with an optional id """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
