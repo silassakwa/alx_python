6cb8154dcb25f65cf7686127944da1f6b595688f
silassakwa <silassakwa2016@gmail.com>
2023-10-01 22:52:18 -0700

    1-export_to_csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -16,16 +16,21 @@ def get_employee_data(employee_id):
         employee_data = employee_response.json()
         todo_data = todo_response.json()
 
+        # Ensure User ID and Username are 25 characters long
+        user_id = f"{employee_id:025}"
+        username = f"{employee_data['username'][:25]:<25}"
+
         # Calculate the number of completed tasks
         completed_tasks = [task for task in todo_data if task["completed"]]
         num_completed_tasks = len(completed_tasks)
         total_num_tasks = len(todo_data)
 
         # Display the employee TODO list progress
+        print(f"User ID and Username: {user_id} - {username}")
         print(f"Employee {employee_data['name']} is done with tasks({num_completed_tasks}/{total_num_tasks}):")
 
         # Create and write the data to a CSV file
-        csv_file_name = f"{employee_id}.csv"
+        csv_file_name = f"{user_id}.csv"
         with open(csv_file_name, mode='w', newline='') as csv_file:
             fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
@@ -36,8 +41,8 @@ def get_employee_data(employee_id):
             # Write each task as a row in the CSV file
             for task in todo_data:
                 writer.writerow({
-                    "USER_ID": employee_id,
-                    "USERNAME": employee_data["username"],
+                    "USER_ID": user_id,
+                    "USERNAME": username,
                     "TASK_COMPLETED_STATUS": task["completed"],
                     "TASK_TITLE": task["title"]
                 })
@@ -50,7 +55,7 @@ def get_employee_data(employee_id):
 if __name__ == "__main__":
     # Check if an employee ID was provided as a command-line argument
     if len(sys.argv) != 2:
-        print("Usage: python3 0-extend_and_export_data.py <employee_id>")
+        print("Usage: python3 script.py <employee_id>")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
