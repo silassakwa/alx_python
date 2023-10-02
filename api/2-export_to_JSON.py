709e61532c8bf4b9c1a2c7c85f0431d2fedaddd9
silassakwa <silassakwa2016@gmail.com>
2023-10-01 22:55:20 -0700

    2-export to jspn

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,48 @@
+import requests
+import sys
+import json
+
+def get_employee_data(employee_id):
+    # Define the API URLs for employee details and their TODO list
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+
+    # Make API requests to fetch employee details and TODO list
+    employee_response = requests.get(employee_url)
+    todo_response = requests.get(todo_url)
+
+    # Check if both requests were successful
+    if employee_response.status_code == 200 and todo_response.status_code == 200:
+        employee_data = employee_response.json()
+        todo_data = todo_response.json()
+
+        # Create a dictionary to store the JSON data
+        json_data = {
+            "USER_ID": [
+                {
+                    "task": task["title"],
+                    "completed": task["completed"],
+                    "username": employee_data["username"]
+                }
+                for task in todo_data
+            ]
+        }
+
+        # Create a JSON file and write the data to it
+        json_file_name = f"{employee_id}.json"
+        with open(json_file_name, 'w') as json_file:
+            json.dump(json_data, json_file, indent=4)
+
+        print(f"Data exported to {json_file_name}")
+
+    else:
+        print(f"Failed to retrieve data for employee ID {employee_id}")
+
+if __name__ == "__main__":
+    # Check if an employee ID was provided as a command-line argument
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_data(employee_id)
